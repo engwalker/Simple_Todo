c09834a77b8b1ba5e480c221e69cb0bc5d59eabb
Jeremy Engman <engwalker@gmail.com>
2015-06-07 10:19:49 -0700

    Added a couple files for sqlite database implementation, and some code in mainactivity.java. I have commented the code out for now until I am ready to implement the db.

--- app/src/main/java/engman/jeremy/simpletodo/MainActivity.java
+++ app/src/main/java/engman/jeremy/simpletodo/MainActivity.java
@@ -2,6 +2,8 @@ package engman.jeremy.simpletodo;
 
 import android.content.ClipData;
 import android.content.ContentValues;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,27 +14,48 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import java.io.IOError;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.prefs.Preferences;
 
 public class MainActivity extends AppCompatActivity {
     //three variables for the ArrayList, an ArrayAdapter, and the ListView
     private ArrayList<String> items;
     private ArrayAdapter<String> itemsAdapter;
     private ListView listViewItems;
+    private String prefKey = "todo.txt";
+    private SharedPreferences sharedPrefs;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //create a handle to SharedPreferences for key-value pair saving
+        SharedPreferences sharedPref = this.getSharedPreferences(prefKey, Context.MODE_PRIVATE);
+
+
         //finds view by id, creates the new arraylist, creates the new ArrayAdapter(bridge between listview and data in list, then sets the adapter for listViewItems
         listViewItems = (ListView) findViewById(R.id.listViewItems);
+
+        //populate the items array on creation
+        //Set<String> set = sharedPref.getStringSet(prefKey, null);
+        //items = new ArrayList<>(set);
         items = new ArrayList<>();
+
         itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
         listViewItems.setAdapter(itemsAdapter);
         //call item removal listener for long press
         longPressListener();
+
+
     }
 
     @Override
@@ -59,6 +82,7 @@ public class MainActivity extends AppCompatActivity {
 
     //new method that is attached to the addItemButton
     public void onClick(View v) {
+
         //new edit text variable, with xml edittext box assigned by id
         EditText editTextNewItem = (EditText) findViewById(R.id.editTextNewItem);
         //new string variable with text in editTextNewItem textbox assigned and converted to string
@@ -66,20 +90,29 @@ public class MainActivity extends AppCompatActivity {
         //Array adapter adds new item to array
         itemsAdapter.add(itemText);
 
-        /** sqlite database code
-        //instantiate the dbhelper class
-        ItemEntryDbHelper itemDbHelper = new ItemEntryDbHelper(this);
-        //gets data repository in write mode
-        SQLiteDatabase database = itemDbHelper.getWritableDatabase();
-        //create new map of values, column names are the keys
-        ContentValues values = new ContentValues();
-        values.put(SQLDatabaseClass.ItemEntry.COLUMN_NAME_ITEM_ID, items.size()+1);
-        values.put(SQLDatabaseClass.ItemEntry.COLUMN_NAME_ITEM_TEXT, itemText);
 
-        //inserts the new row in the database, returning the primary key value of the new row
-        long newRowId;
-        newRowId = database.insert(SQLDatabaseClass.ItemEntry.TABLE_NAME, null, values);
-        */
+
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+        editor.putString(prefKey, items.get(items.size() -1));
+        editor.commit();
+        /** sqlite database code
+         //instantiate the dbhelper class
+         ItemEntryDbHelper itemDbHelper = new ItemEntryDbHelper(this);
+         //gets data repository in write mode
+         SQLiteDatabase database = itemDbHelper.getWritableDatabase();
+         //create new map of values, column names are the keys
+         ContentValues values = new ContentValues();
+         values.put(SQLDatabaseClass.ItemEntry.COLUMN_NAME_ITEM_ID, items.size()+1);
+         values.put(SQLDatabaseClass.ItemEntry.COLUMN_NAME_ITEM_TEXT, itemText);
+
+         //inserts the new row in the database, returning the primary key value of the new row
+         long newRowId;
+         newRowId = database.insert(SQLDatabaseClass.ItemEntry.TABLE_NAME, null, values);
+         */
+
+        //write to sharedPreferences file
+
+
 
         //blanks the editTextNewItem text box since it's been added to the list
         editTextNewItem.setText("");
@@ -93,12 +126,23 @@ public class MainActivity extends AppCompatActivity {
                 //remove item in the items array at position that has been long pressed
                 items.remove(position);
                 //refresh the adapter
-               itemsAdapter.notifyDataSetChanged();
+                itemsAdapter.notifyDataSetChanged();
                 //return true to show that the even has been handled
                 return true;
             }
         });
     }
-
-
+/**
+    //called when app is off screen, before onDestroy
+    @Override
+    public void onStop() {
+        super.onStop();
+        //write array to SharedPreferences file
+        SharedPreferences sharedPref = this.getSharedPreferences(prefKey, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        Set<String> set = new HashSet<String>();
+        set.addAll(items);
+            editor.putStringSet(prefKey, set);
+            editor.commit();
+    }*/
 }
