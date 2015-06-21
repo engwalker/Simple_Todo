592c97b620967b6c19cca49a9baaa204da6e24f7
Jeremy Engman <engwalker@gmail.com>
2015-06-20 17:32:39 -0700

    ArrayList now saves to an SQLite database. Populates ArrayList onCreate, and deletes items on long press.

--- app/src/main/java/engman/jeremy/simpletodo/MainActivity.java
+++ app/src/main/java/engman/jeremy/simpletodo/MainActivity.java
@@ -1,5 +1,8 @@
 package engman.jeremy.simpletodo;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,6 +12,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -18,19 +22,43 @@ public class MainActivity extends AppCompatActivity {
     private ArrayAdapter<String> itemsAdapter;
     private ListView listViewItems;
 
+    //declare database, cursor, and length variables
+    SQLiteDatabase db;
+    Cursor c;
+    int dbLength;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //finds view by id, creates the new arraylist, creates the new ArrayAdapter(bridge between listview and data in list, then sets the adapter for listViewItems
+        //finds view by id, creates the new arraylist
         listViewItems = (ListView) findViewById(R.id.listViewItems);
         items = new ArrayList<>();
-        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
-        listViewItems.setAdapter(itemsAdapter);
+
         //call item removal listener for long press
         longPressListener();
 
+        //create (if not exist) or open database and table
+        db = openOrCreateDatabase("ItemDB", Context.MODE_PRIVATE, null);
+        //db.execSQL("CREATE TABLE IF NOT EXISTS item(itemnum INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, itemtext VARCHAR);");
+        db.execSQL("CREATE TABLE IF NOT EXISTS item(itemid INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, itemtext VARCHAR);");
+
+        //runs an SQL query selecting all from table item
+        c = db.rawQuery("SELECT * FROM item", null);
+        //populate arrayList if there are items in the database
+        dbLength = c.getCount();
+        //c.movetofirst not working?
+        if (dbLength != 0 )
+        {
+            while (c.moveToNext()) {
+                items.add(c.getString(1));
+            }
+        }
+
+        // defines ArrayAdapter(bridge between listview and data in list, then sets the adapter for listViewItems
+        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        listViewItems.setAdapter(itemsAdapter);
     }
 
     @Override
@@ -63,6 +91,9 @@ public class MainActivity extends AppCompatActivity {
         String itemText = editTextNewItem.getText().toString();
         //Array adapter adds new item to array
         itemsAdapter.add(itemText);
+
+        db.execSQL("INSERT INTO item VALUES(NULL,'" + itemText + "');");
+
         //blanks the editTextNewItem text box since it's been added to the list
         editTextNewItem.setText("");
     }
@@ -72,6 +103,10 @@ public class MainActivity extends AppCompatActivity {
         listViewItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+                //delete item from list that matches itemText. Gives possibilities of duplicates, need to revise
+                db.execSQL("DELETE FROM item WHERE itemtext='" + items.get(position) +"'");
+
                 //remove item in the items array at position that has been long pressed
                 items.remove(position);
                 //refresh the adapter
@@ -80,6 +115,12 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+    }
+
+    //onStop called as app goes off screen, before onDestroy. Closing database
+    public void onStop() {
+        super.onStop();
+        db.close();
     }
 
 
